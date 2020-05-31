bfe37edb6a2fb2136ff064f2ec5fd09e3c707d92
Bruno Eiji <brunomine151@outlook.com>
2020-05-31 15:36:33 -0300

    Criação de Bancos de dados em Arquivos

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,406 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.lblTel = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.mtbTell = new System.Windows.Forms.MaskedTextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.mtbCpf = new System.Windows.Forms.MaskedTextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabCadastro = new System.Windows.Forms.TabPage();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.tabListagem = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dgvBanco = new System.Windows.Forms.DataGridView();
+            this.clnEmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clnName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clnCpf = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clnPassword = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabCadastro.SuspendLayout();
+            this.tabListagem.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBanco)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(20, 113);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.ForeColor = System.Drawing.Color.White;
+            this.lblEmail.Location = new System.Drawing.Point(20, 49);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 0;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.ForeColor = System.Drawing.Color.White;
+            this.lblCPF.Location = new System.Drawing.Point(20, 175);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(23, 13);
+            this.lblCPF.TabIndex = 0;
+            this.lblCPF.Text = "Cpf";
+            // 
+            // lblTel
+            // 
+            this.lblTel.AutoSize = true;
+            this.lblTel.ForeColor = System.Drawing.Color.White;
+            this.lblTel.Location = new System.Drawing.Point(144, 175);
+            this.lblTel.Name = "lblTel";
+            this.lblTel.Size = new System.Drawing.Size(24, 13);
+            this.lblTel.TabIndex = 0;
+            this.lblTel.Text = "Tell";
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.Color.MidnightBlue;
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtName.ForeColor = System.Drawing.Color.White;
+            this.txtName.Location = new System.Drawing.Point(23, 129);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(118, 13);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.MidnightBlue;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.ForeColor = System.Drawing.Color.White;
+            this.txtPassword.Location = new System.Drawing.Point(164, 129);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(88, 13);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.Tag = "";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BackColor = System.Drawing.Color.MidnightBlue;
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtEmail.ForeColor = System.Drawing.Color.White;
+            this.txtEmail.Location = new System.Drawing.Point(23, 69);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(229, 13);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.ForeColor = System.Drawing.Color.White;
+            this.lblPassword.Location = new System.Drawing.Point(161, 113);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password";
+            // 
+            // mtbTell
+            // 
+            this.mtbTell.BackColor = System.Drawing.Color.MidnightBlue;
+            this.mtbTell.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.mtbTell.ForeColor = System.Drawing.Color.White;
+            this.mtbTell.Location = new System.Drawing.Point(147, 191);
+            this.mtbTell.Mask = "(00) 00000 - 0000";
+            this.mtbTell.Name = "mtbTell";
+            this.mtbTell.Size = new System.Drawing.Size(105, 13);
+            this.mtbTell.TabIndex = 5;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(147, 235);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(93, 37);
+            this.btnSalvar.TabIndex = 7;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(32, 235);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(91, 37);
+            this.btnLimpar.TabIndex = 6;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(523, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(48, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 21);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "CREATE AN ACCOUNT";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(23, 84);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(219, 1);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(23, 144);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(115, 1);
+            this.panel2.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(165, 144);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(77, 1);
+            this.panel3.TabIndex = 9;
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // mtbCpf
+            // 
+            this.mtbCpf.BackColor = System.Drawing.Color.MidnightBlue;
+            this.mtbCpf.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.mtbCpf.ForeColor = System.Drawing.Color.White;
+            this.mtbCpf.Location = new System.Drawing.Point(23, 191);
+            this.mtbCpf.Mask = "000.000.000-00";
+            this.mtbCpf.Name = "mtbCpf";
+            this.mtbCpf.Size = new System.Drawing.Size(100, 13);
+            this.mtbCpf.TabIndex = 4;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabCadastro);
+            this.tabControl1.Controls.Add(this.tabListagem);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(514, 315);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabCadastro
+            // 
+            this.tabCadastro.BackColor = System.Drawing.Color.MidnightBlue;
+            this.tabCadastro.Controls.Add(this.panel4);
+            this.tabCadastro.Controls.Add(this.label1);
+            this.tabCadastro.Controls.Add(this.mtbCpf);
+            this.tabCadastro.Controls.Add(this.lblName);
+            this.tabCadastro.Controls.Add(this.panel3);
+            this.tabCadastro.Controls.Add(this.lblEmail);
+            this.tabCadastro.Controls.Add(this.panel2);
+            this.tabCadastro.Controls.Add(this.lblCPF);
+            this.tabCadastro.Controls.Add(this.panel1);
+            this.tabCadastro.Controls.Add(this.lblTel);
+            this.tabCadastro.Controls.Add(this.txtName);
+            this.tabCadastro.Controls.Add(this.txtPassword);
+            this.tabCadastro.Controls.Add(this.btnLimpar);
+            this.tabCadastro.Controls.Add(this.txtEmail);
+            this.tabCadastro.Controls.Add(this.btnSalvar);
+            this.tabCadastro.Controls.Add(this.lblPassword);
+            this.tabCadastro.Controls.Add(this.mtbTell);
+            this.tabCadastro.Location = new System.Drawing.Point(4, 22);
+            this.tabCadastro.Name = "tabCadastro";
+            this.tabCadastro.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCadastro.Size = new System.Drawing.Size(506, 289);
+            this.tabCadastro.TabIndex = 0;
+            this.tabCadastro.Text = "Cadastro";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel4.BackgroundImage")));
+            this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel4.Location = new System.Drawing.Point(258, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(252, 289);
+            this.panel4.TabIndex = 10;
+            // 
+            // tabListagem
+            // 
+            this.tabListagem.Controls.Add(this.button2);
+            this.tabListagem.Controls.Add(this.dgvBanco);
+            this.tabListagem.Location = new System.Drawing.Point(4, 22);
+            this.tabListagem.Name = "tabListagem";
+            this.tabListagem.Padding = new System.Windows.Forms.Padding(3);
+            this.tabListagem.Size = new System.Drawing.Size(506, 289);
+            this.tabListagem.TabIndex = 1;
+            this.tabListagem.Text = "Listagem";
+            this.tabListagem.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(407, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Importar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dgvBanco
+            // 
+            this.dgvBanco.BackgroundColor = System.Drawing.Color.MidnightBlue;
+            this.dgvBanco.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBanco.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clnEmail,
+            this.clnName,
+            this.clnCpf,
+            this.clnPassword});
+            this.dgvBanco.Location = new System.Drawing.Point(6, 6);
+            this.dgvBanco.Name = "dgvBanco";
+            this.dgvBanco.Size = new System.Drawing.Size(491, 277);
+            this.dgvBanco.TabIndex = 0;
+            // 
+            // clnEmail
+            // 
+            this.clnEmail.HeaderText = "Email";
+            this.clnEmail.Name = "clnEmail";
+            this.clnEmail.Width = 150;
+            // 
+            // clnName
+            // 
+            this.clnName.FillWeight = 120F;
+            this.clnName.HeaderText = "Name";
+            this.clnName.Name = "clnName";
+            // 
+            // clnCpf
+            // 
+            this.clnCpf.HeaderText = "Cpf";
+            this.clnCpf.Name = "clnCpf";
+            // 
+            // clnPassword
+            // 
+            this.clnPassword.HeaderText = "Password";
+            this.clnPassword.Name = "clnPassword";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(550, 339);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabCadastro.ResumeLayout(false);
+            this.tabCadastro.PerformLayout();
+            this.tabListagem.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBanco)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.Label lblTel;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.MaskedTextBox mtbTell;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.MaskedTextBox mtbCpf;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabCadastro;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TabPage tabListagem;
+        private System.Windows.Forms.DataGridView dgvBanco;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnEmail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnCpf;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnPassword;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.IO; 
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static String path = "C:/git/Banco De Dados";
+        static String file = path + "/Banco.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtEmail.Text = "";
+            txtName.Text = "";
+            txtPassword.Text = "";
+            mtbTell.Text = "";
+            mtbCpf.Text = "";
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            int Banco = 0;
+
+            if(txtEmail.Text == "")
+            {
+                //Validação Convêncional
+                //MessageBox.Show("Email Obrigatório");
+                errorProvider.SetError(txtEmail, "Mandatory Email");
+
+                Banco = 1;
+            }
+            else
+            {
+                errorProvider.SetError(txtEmail, "");
+
+                Banco = 0;
+            }
+            if(txtName.Text == "")
+            {
+                errorProvider.SetError(txtName, "Name Required");
+
+                Banco = 1;
+            }
+            else
+            {
+                errorProvider.SetError(txtName, "");
+
+                Banco = 0;
+
+            }
+            if (txtPassword.Text == "")
+            {
+                errorProvider.SetError(txtPassword, "Password Required");
+
+                Banco = 1;
+            } 
+           else
+            {
+             errorProvider.SetError(txtPassword, "");
+
+                Banco = 0;
+
+            }
+
+            if(Banco == 1)
+            {
+                MessageBox.Show("Please fill in the required fields !");
+            }
+
+            else
+            {
+                bool checkDirExist = Directory.Exists(path);
+                if (!checkDirExist)
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                bool checkFileExist = File.Exists(file);
+
+                String line = txtEmail.Text + "|" + txtName.Text + "|" + mtbCpf.Text + "|" + txtPassword.Text + "|" + mtbTell.Text + "|";
+
+                if (!checkFileExist)
+                {
+                    using (StreamWriter sw = File.CreateText(file))
+                    {
+                        sw.WriteLine(line);
+                    }
+                }
+                else
+                {
+                    using (StreamWriter sw = File.AppendText(file))
+                    {
+                        sw.WriteLine(line);
+                    }
+                }
+            }
+
+            
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+    }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(file))
+            {
+                dgvBanco.Rows.Clear();
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    int line = 0;
+                    string In;
+
+                    while ((In = sr.ReadLine()) != null)
+                    {
+                        //Console.WriteLine(In);
+
+                        //MessageBox.Show(In);
+                        string[] fields = In.Split('|');
+
+                        dgvBanco.Rows.Add(fields[0], fields[1], fields[2], fields[3]);
+
+                        line++;
+                    }
+                    sr.Close();
+
+                }
+            }
+            /*
+            else
+            {
+                MessageBox.Show("Nenhum Arquivo encontrado");
+            }
+            */
+        }
+    }
+}
